53bffb551f0df9b5851fe45bebe527613f1733d8
RuLe0G <khlebnikov2015@yandex.ru>
2023-06-23 17:13:45 +0700

    Update search.js

--- search.js
+++ search.js
@@ -5,26 +5,22 @@ xhr.onload = function() {
     const data = JSON.parse(xhr.responseText);
     const suggestions = data.applist.apps;
     
-    // Получите ссылку на элементы в HTML
     const searchInput = document.getElementById('search-input');
     const suggestionsList = document.getElementById('suggestions-list');
+    let selectedAppId;
 
-    // Слушайте событие ввода текста в поле input
     searchInput.addEventListener('input', () => {
-      console.log("in!");
       const userInput = searchInput.value.toLowerCase();
       const filteredSuggestions = suggestions.filter(suggestion => suggestion.name.toLowerCase().startsWith(userInput));
 
-      // Очистите список подсказок
       suggestionsList.innerHTML = '';
 
-      // Добавьте отфильтрованные подсказки в список
-      filteredSuggestions.forEach(suggestion => {
+      limitedSuggestions.forEach(suggestion => {
         const li = document.createElement('li');
         li.textContent = suggestion.name;
         li.addEventListener('click', () => {
-          // Установите выбранную подсказку в поле ввода
           searchInput.value = suggestion.name;
+          selectedAppId = suggestion.appid;
           suggestionsList.innerHTML = '';
         });
         suggestionsList.appendChild(li);
@@ -33,3 +29,31 @@ xhr.onload = function() {
   }
 };
 xhr.send();
+
+const xhrH = new XMLHttpRequest();
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', () => {
+  xhrH.open('GET', `https://steamspy.com/api.php?request=appdetails&appid=${selectedAppId}`, true);
+  xhrH.onload = function() {
+    if (xhrH.status === 200) {
+      const appDetails = JSON.parse(xhrH.responseText);
+
+      // Получите ссылку на первый блок в HTML
+      const firstInfoBlock = document.querySelector('.info-blocks .info-block');
+
+      // Очистите содержимое первого блока
+      firstInfoBlock.innerHTML = '';
+
+      // Создайте элементы для отображения информации
+      const title = document.createElement('h2');
+      title.textContent = appDetails.name;
+      const description = document.createElement('p');
+      description.textContent = appDetails.description;
+
+      // Добавьте элементы в первый блок
+      firstInfoBlock.appendChild(title);
+      firstInfoBlock.appendChild(description);
+    }
+  };
+  xhrH.send();
+});
