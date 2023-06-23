4289e9ea534b647133da33657e1a71c8eaecc09d
RuLe0G <khlebnikov2015@yandex.ru>
2023-06-23 16:25:50 +0700

    try gg

---
+++ search.js
@@ -0,0 +1,34 @@
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '/steam_data.json', true);
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    const data = JSON.parse(xhr.responseText);
+    const suggestions = data.applist.apps;
+    
+    // Получите ссылку на элементы в HTML
+    const searchInput = document.getElementById('search-input');
+    const suggestionsList = document.getElementById('suggestions-list');
+
+    // Слушайте событие ввода текста в поле input
+    searchInput.addEventListener('input', () => {
+      const userInput = searchInput.value.toLowerCase();
+      const filteredSuggestions = suggestions.filter(suggestion => suggestion.name.toLowerCase().startsWith(userInput));
+
+      // Очистите список подсказок
+      suggestionsList.innerHTML = '';
+
+      // Добавьте отфильтрованные подсказки в список
+      filteredSuggestions.forEach(suggestion => {
+        const li = document.createElement('li');
+        li.textContent = suggestion.name;
+        li.addEventListener('click', () => {
+          // Установите выбранную подсказку в поле ввода
+          searchInput.value = suggestion.name;
+          suggestionsList.innerHTML = '';
+        });
+        suggestionsList.appendChild(li);
+      });
+    });
+  }
+};
+xhr.send();
